1f2a092534cacf369137bd3dabde600eb7036d0a
Yves Courtois <Courtois1337@gmail.com>
2014-02-16 12:40:07 -0600

    working mostly now

--- includes/ready.js
+++ includes/ready.js
@@ -1,171 +1,165 @@
 $(document).ready(function(){
-	$('#convertBtn').on('click', function(e){
-		e.preventDefault();
 
-		var tmp = $('#pronouns').val();
-		if(tmp=="") {
+	$('#convertBtn').on('click', function(e){
+		
+e.preventDefault();
+
+		var chosen_option = $('#pronouns').val();
+		if(chosen_option=="") {
 			alert('Please select an option from the dropdown menu');
 		}
 		else {
-			console.log(tmp);
-			var he = {
-				nomative: "he",
-				oblique: "him", 
-				possessive_determiner: "his",
-				possessive_pronoun: "his",
-				reflexive: "himself"
+			console.log(chosen_option);
+
+			// used to check if a pronoun is matched
+			var isMatch = false;
+			var end_char = "";
+			var word_minus_end_char = "";
+			var isTheyChosen = false;
+
+			var he = ["he", "him", "his", "his", "himself"];
+			var she = ["she", "her", "hers", "hers", "herself"];
+			//gender neutral pronouns
+			var they =  ["they", "them", "their", "theirs", "themself"];
+			var ze_zir= ["ze", "zir", "zir", "zirs", "zirself"];
+			var ze_hir= ["ze", "hir", "hir", "hirs", "hirself"];
+			var xe =    ["xe", "xem", "xyr", "xyrs", "xemself"];
+			//make this equal the chosen pronounss
+			var chosen_pronouns = [];
+
+			if(chosen_option=="they_them") {
+				chosen_pronouns = they;
+				isTheyChosen = true;
+			}
+			else if(chosen_option=="ze_zir") {
+				chosen_pronouns = ze_zir;
+			}
+			else if(chosen_option=="ze_hir") {
+				chosen_pronouns = ze_hir;
+			}
+			else if(chosen_option=="xe_xir") {
+				chosen_pronouns = xe;
 			}
 
-			var she = {
-				nomative: "she",
-				oblique: "her", 
-				possessive_determiner: "hers",
-				possessive_pronoun: "hers",
-				reflexive: "herself"
-			}
-			
-			if(tmp=="they_them") {
-				// they / them
-				var they = {
-					nomative: "they",
-					oblique: "them", 
-					possessive_determiner: "their",
-					possessive_pronoun: "theirs",
-					reflexive: "themself"
-				}
 
-			}
-			else if(tmp=="ze_zir") {
-				// ze / zir
-				var ze_zir = {
-					nomative: "ze",
-					oblique: "zir", 
-					possessive_determiner: "zir",
-					possessive_pronoun: "zirs",
-					reflexive: "zirself"
-				}
 
-				var aoeu = $("#originaltext").val();
+			// split the text into words
+			var words = $("#originaltext").val().split(" ");
 
-				var regex_he_nomative = new RegExp(he.nomative, "g");
-				var regex_he_oblique = new RegExp(he.oblique, "g");
-				var regex_he_possessive_determiner = new RegExp(he.possessive_determiner, "g");
-				var regex_he_possessive_pronoun = new RegExp(he.possessive_pronoun, "g");
-				var regex_he_reflexive = new RegExp(he.reflexive, "g");
-				$("#convertedtext").val(   aoeu.replace(regex_he_nomative,                ze_zir.nomative              ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_oblique,                 ze_zir.oblique               ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_possessive_determiner,   ze_zir.possessive_determiner ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_possessive_pronoun,      ze_zir.possessive_pronoun    ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_reflexive,               ze_zir.reflexive             ) );
+			for(var i = 0; i < words.length; ++i) {
+				for(var j = 0; j< chosen_pronouns.length; ++j) {
+					// check for direct matching
+					if(words[i] == he[j] || words[i] == she[j]) {
+						words[i] = chosen_pronouns[j];
+						isMatch = true;
+					}
+					// check to see if a word is capitalized and compare to "he"
+					else if(words[i].substring(0,1).toLowerCase() == he[j].substring(0,1) &&
+							words[i].toLowerCase() == he[j]) {
+						words[i] = chosen_pronouns[j].substring(0, 1).toUpperCase() + chosen_pronouns[j].substring(1);
+						isMatch = true;
+					}
+					// check to see if a word is capitalized and compare to "she"
+					else if(words[i].substring(0,1).toLowerCase() == she[j].substring(0,1) &&
+							words[i].toLowerCase() == she[j]) {
+						words[i] = chosen_pronouns[j].substring(0, 1).toUpperCase() + chosen_pronouns[j].substring(1);
+						isMatch = true;
+					}
+					// check to see if a word is followed by a special sentence character
+					else if (words[i].substring(words.length - 1) == "." ||
+							words[i].substring(words.length - 1)  == "," ||
+							words[i].substring(words.length - 1)  == "?" ||
+							words[i].substring(words.length - 1)  == "!" ) {
 
-				var regex_she_nomative = new RegExp(she.nomative, "g");
-				var regex_she_oblique = new RegExp(she.oblique, "g");
-				var regex_she_possessive_determiner = new RegExp(she.possessive_determiner, "g");
-				var regex_she_possessive_pronoun = new RegExp(she.possessive_pronoun, "g");
-				var regex_she_reflexive = new RegExp(she.reflexive, "g");
+						end_char = words[i].substring(words.length - 1);
+						word_minus_end_char = words[i].substring(0,words.length - 1);
 
-				$("#convertedtext").val(   aoeu.replace(regex_she_nomative,                ze_zir.nomative              ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_oblique,                 ze_zir.oblique               ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_possessive_determiner,   ze_zir.possessive_determiner ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_possessive_pronoun,      ze_zir.possessive_pronoun    ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_reflexive,               ze_zir.reflexive             ) );
-		
-			}
-			else if(tmp=="ze_hir") {
-				// ze / hir
-				var ze_hir = {
-					nomative: "ze",
-					oblique: "hir", 
-					possessive_determiner: "hir",
-					possessive_pronoun: "hirs",
-					reflexive: "hirself"
-				}
+						if(words[i] == he[j] || words[i] == she[j]) {
+							words[i] = chosen_pronouns[j] + end_char;
+							isMatch = true;
+						}
+						else if(words[i].substring(0,1).toLowerCase() == he[j].substring(0,1) &&
+								words[i].toLowerCase() == he[j]) {
+							words[i] = chosen_pronouns[j].substring(0, 1).toUpperCase() + chosen_pronouns[j].substring(1) + end_char;
+							isMatch = true;
+						}
+						else if(words[i].substring(0,1).toLowerCase() == she[j].substring(0,1) &&
+								words[i].toLowerCase() == she[j]) {
+							words[i] = chosen_pronouns[j].substring(0, 1).toUpperCase() + chosen_pronouns[j].substring(1) + end_char;
+							isMatch = true;
+						}
+					}
 
-				var aoeu = $("#originaltext").val();
+					// if a word has been matched
+					if(isMatch) {
+						isMatch = false;
+						// if anything but "they" is chosen gender-neutral pronoun
+						if( isTheyChosen) {
+							if(i< words.length-1) {
+								console.log("next word: "+words[i+1]);
+							}
+							if(i< words.length-1 && words[i+1]=="is") {
+								console.log("\treplaced is");
+								words[i+1] = "are";
+								break;
+							}
+							else if (i< words.length-1 && words[i+1] == "was") {
+								console.log("\treplaced was");
+								words[i+1] = "were";
+								break;
+							}
+							else if (i< words.length-1 && words[i+1] == "has") {
+								console.log("\treplaced has");
+								words[i+1] = "have";
+								break;
+							}
+							else if(i< words.length-1 && words[i+1].substring(words[i+1].length - 1) =="s" ) {
+								console.log("-");
+								words[i+1] = words[i+1].substring(0,words[i+1].length-1);
+							}
+							else if (i< words.length-1 &&
+								words[i+1].substring(words[i+1].length - 1) == "." ||
+								words[i+1].substring(words[i+1].length - 1)  == "," ||
+								words[i+1].substring(words[i+1].length - 1)  == "?" ||
+								words[i+1].substring(words[i+1].length - 1)  == "!" ) {
+								console.log("has punc");
 
-				var regex_he_nomative = new RegExp(he.nomative, "g");
-				var regex_he_oblique = new RegExp(he.oblique, "g");
-				var regex_he_possessive_determiner = new RegExp(he.possessive_determiner, "g");
-				var regex_he_possessive_pronoun = new RegExp(he.possessive_pronoun, "g");
-				var regex_he_reflexive = new RegExp(he.reflexive, "g");
-				$("#convertedtext").val(   aoeu.replace(regex_he_nomative,                ze_hir.nomative              ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_oblique,                 ze_hir.oblique               ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_possessive_determiner,   ze_hir.possessive_determiner ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_possessive_pronoun,      ze_hir.possessive_pronoun    ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_reflexive,               ze_hir.reflexive             ) );
+								end_char = words[i+1].substring(words[i+1].length - 1);
+								word_minus_end_char = words[i+1].substring(0,words[i+1].length - 1);
 
-				var regex_she_nomative = new RegExp(she.nomative, "g");
-				var regex_she_oblique = new RegExp(she.oblique, "g");
-				var regex_she_possessive_determiner = new RegExp(she.possessive_determiner, "g");
-				var regex_she_possessive_pronoun = new RegExp(she.possessive_pronoun, "g");
-				var regex_she_reflexive = new RegExp(she.reflexive, "g");
+								console.log("end_char: "+end_char);
+								console.log("word_minus_end_char: "+word_minus_end_char);
 
-				$("#convertedtext").val(   aoeu.replace(regex_she_nomative,                ze_hir.nomative              ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_oblique,                 ze_hir.oblique               ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_possessive_determiner,   ze_hir.possessive_determiner ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_possessive_pronoun,      ze_hir.possessive_pronoun    ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_reflexive,               ze_hir.reflexive             ) );
+								if (word_minus_end_char == "is") {
+									words[i+1] = "are" + end_char;									
+								}
+								else if (word_minus_end_char == "was") {
+									words[i+1] = "were" + end_char;
+									break;
+								}
+								else if (word_minus_end_char == "has") {
+									words[i+1] = "have" + end_char;
+									break;
+								}
+								else if(i< words.length-1 && word_minus_end_char.substring(word_minus_end_char.length - 1) =="s" ) {
+									console.log("aoeu");
+									words[i+1] = word_minus_end_char.substring(0,word_minus_end_char.length-1);
+								}
+								else {
+									console.log("next word doesn't need to be changed");
+								}
+							}
+						}
+						// if anything but "they" is chosen gender-neutral pronoun
+						else {
+							break;
+						}
 
-			}
-			else if(tmp=="xe_xir") {
-				// xe
-				var xe = {
-					nomative: "xe",
-					oblique: "xem", 
-					possessive_determiner: "xyr",
-					possessive_pronoun: "xyrs",
-					reflexive: "xemself"
-				}
+					}
+				} // inner loop
+			} // outer loop close
 
-				var aoeu = $("#originaltext").val();
-
-				var regex_he_nomative = new RegExp(he.nomative, "g");
-				var regex_he_oblique = new RegExp(he.oblique, "g");
-				var regex_he_possessive_determiner = new RegExp(he.possessive_determiner, "g");
-				var regex_he_possessive_pronoun = new RegExp(he.possessive_pronoun, "g");
-				var regex_he_reflexive = new RegExp(he.reflexive, "g");
-				$("#convertedtext").val(   aoeu.replace(regex_he_nomative,                xe.nomative              ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_oblique,                 xe.oblique               ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_possessive_determiner,   xe.possessive_determiner ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_possessive_pronoun,      xe.possessive_pronoun    ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_he_reflexive,               xe.reflexive             ) );
-
-				var regex_she_nomative = new RegExp(she.nomative, "g");
-				var regex_she_oblique = new RegExp(she.oblique, "g");
-				var regex_she_possessive_determiner = new RegExp(she.possessive_determiner, "g");
-				var regex_she_possessive_pronoun = new RegExp(she.possessive_pronoun, "g");
-				var regex_she_reflexive = new RegExp(she.reflexive, "g");
-
-				$("#convertedtext").val(   aoeu.replace(regex_she_nomative,                xe.nomative              ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_oblique,                 xe.oblique               ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_possessive_determiner,   xe.possessive_determiner ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_possessive_pronoun,      xe.possessive_pronoun    ) );
-				aoeu = $("#convertedtext").val();
-				$("#convertedtext").val(   aoeu.replace(regex_she_reflexive,               xe.reflexive             ) );
-			}
+			$('#convertedtext').val(words.join(" "));
 		}
 	});
 });
